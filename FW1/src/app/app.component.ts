be67b33dea5d68726fdabad17b8c8a5625bdabc8
Nguyễn Đức Huy - PH33852 <139211478+duchuy112233@users.noreply.github.com>
2024-05-08 10:37:57 +0700

    LAB1

--- FW1/src/app/app.component.ts
+++ FW1/src/app/app.component.ts
@@ -1,13 +1,79 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CommonModule, NgFor } from '@angular/common';
+import { Product } from '../Types/Products';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'WEB208-base';
+  title = 'WEB208 FPT POLY';
+  student = {
+    name: 'Nguyễn Đức Huy',
+    birthday: '5/4/2004',
+    msv: 'ph33852',
+  };
+
+  type = [];
+
+  products: Product[] = [
+    {
+      id: 1,
+      title: 'iPhone 9',
+      description: 'An apple mobile which is nothing like apple',
+      price: 549,
+      discountPercentage: 12.96,
+      rating: 4.69,
+      stock: 94,
+      brand: 'Apple',
+      category: 'smartphones',
+      thumbnail: 'https://cdn.dummyjson.com/product-images/1/thumbnail.jpg',
+      images: [
+        'https://cdn.dummyjson.com/product-images/1/1.jpg',
+        'https://cdn.dummyjson.com/product-images/1/2.jpg',
+        'https://cdn.dummyjson.com/product-images/1/3.jpg',
+        'https://cdn.dummyjson.com/product-images/1/4.jpg',
+        'https://cdn.dummyjson.com/product-images/1/thumbnail.jpg',
+      ],
+    },
+    {
+      id: 2,
+      title: 'Samsung Universe 9',
+      description:
+        "Samsung's new variant which goes beyond Galaxy to the Universe",
+      price: 1249,
+      discountPercentage: 15.46,
+      rating: 4.09,
+      stock: 36,
+      brand: 'Samsung',
+      category: 'smartphones',
+      thumbnail: 'https://cdn.dummyjson.com/product-images/3/thumbnail.jpg',
+      images: ['https://cdn.dummyjson.com/product-images/3/1.jpg'],
+    },
+    {
+      id: 4,
+      title: "OPPOF19",
+      description: "OPPO F19 is officially announced on April 2021.",
+      price: 280,
+      discountPercentage: 17.91,
+      rating: 4.3,
+      stock: 123,
+      brand: "OPPO",
+      category: "smartphones",
+      thumbnail: "https://cdn.dummyjson.com/product-images/4/thumbnail.jpg",
+      images: [
+        "https://cdn.dummyjson.com/product-images/4/1.jpg",
+        "https://cdn.dummyjson.com/product-images/4/2.jpg",
+        "https://cdn.dummyjson.com/product-images/4/3.jpg",
+        "https://cdn.dummyjson.com/product-images/4/4.jpg",
+        "https://cdn.dummyjson.com/product-images/4/thumbnail.jpg"
+      ]
+    },
+    
+  ];
+
+  
 }
