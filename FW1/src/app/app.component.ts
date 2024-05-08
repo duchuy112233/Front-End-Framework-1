416470b9803183a8b041d30b8ca4bc7d68bba0b9
Nguyễn Đức Huy - PH33852 <139211478+duchuy112233@users.noreply.github.com>
2024-05-08 10:54:12 +0700

    LAB_1
    LAB1

--- FW1/src/app/app.component.ts
+++ FW1/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule, NgFor } from '@angular/common';
 import { Product } from '../Types/Products';
+import { IProduct } from '../Types/Bai4';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, NgFor],
+
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
@@ -55,25 +57,81 @@ export class AppComponent {
     },
     {
       id: 4,
-      title: "OPPOF19",
-      description: "OPPO F19 is officially announced on April 2021.",
+      title: 'OPPOF19',
+      description: 'OPPO F19 is officially announced on April 2021.',
       price: 280,
       discountPercentage: 17.91,
       rating: 4.3,
       stock: 123,
-      brand: "OPPO",
-      category: "smartphones",
-      thumbnail: "https://cdn.dummyjson.com/product-images/4/thumbnail.jpg",
+      brand: 'OPPO',
+      category: 'smartphones',
+      thumbnail: 'https://cdn.dummyjson.com/product-images/4/thumbnail.jpg',
       images: [
-        "https://cdn.dummyjson.com/product-images/4/1.jpg",
-        "https://cdn.dummyjson.com/product-images/4/2.jpg",
-        "https://cdn.dummyjson.com/product-images/4/3.jpg",
-        "https://cdn.dummyjson.com/product-images/4/4.jpg",
-        "https://cdn.dummyjson.com/product-images/4/thumbnail.jpg"
-      ]
+        'https://cdn.dummyjson.com/product-images/4/1.jpg',
+        'https://cdn.dummyjson.com/product-images/4/2.jpg',
+        'https://cdn.dummyjson.com/product-images/4/3.jpg',
+        'https://cdn.dummyjson.com/product-images/4/4.jpg',
+        'https://cdn.dummyjson.com/product-images/4/thumbnail.jpg',
+      ],
     },
-    
   ];
 
-  
+  productss: IProduct[] = [
+    {
+      productId: 1,
+      productName: 'Leaf Rake',
+      productCode: 'GDN-0011',
+      releaseDate: 'March 19, 2016',
+      description: 'Leaf rake with 48-inch wooden handle.',
+      price: 19.95,
+      starRating: 3.2,
+      imageUrl:
+        'http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png',
+    },
+    {
+      productId: 2,
+
+      productName: 'Garden Cart',
+      productCode: 'GDN-0023',
+      releaseDate: 'March 18, 2016',
+      description: '15 gallon capacity rolling garden cart',
+      price: 32.99,
+      starRating: 4.2,
+      imageUrl:
+        'http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png',
+    },
+    {
+      productId: 5,
+      productName: 'Hammer',
+      productCode: 'TBX-0048',
+      releaseDate: 'May 21, 2016',
+      description: 'Curved claw steel hammer',
+      price: 8.9,
+      starRating: 4.8,
+      imageUrl:
+        'http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png',
+    },
+    {
+      productId: 8,
+      productName: 'Saw',
+      productCode: 'TBX-0022',
+      releaseDate: 'May 15, 2016',
+      description: '15-inch steel blade hand saw',
+      price: 11.55,
+      starRating: 3.7,
+      imageUrl:
+        'http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png',
+    },
+    {
+      productId: 10,
+      productName: 'Video Game Controller',
+      productCode: 'GMG-0042',
+      releaseDate: 'October 15, 2015',
+      description: 'Standard two-button video game controller',
+      price: 35.95,
+      starRating: 4.6,
+      imageUrl:
+        'http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png',
+    },
+  ];
 }
