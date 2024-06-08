0bb17649152dec9337253e4cbe4dd15c4fde8c2d
Nguyễn Đức Huy - PH33852 <139211478+duchuy112233@users.noreply.github.com>
2024-06-08 09:23:13 +0700

    .

--- BASE - CRUD/PH33852/src/app/pages/product-list/product-list.component.ts
+++ BASE - CRUD/PH33852/src/app/pages/product-list/product-list.component.ts
@@ -12,29 +12,29 @@ import { CommonModule } from '@angular/common';
   styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
-  products: Product[] = [];
-  filteredProducts: Product[] = [];
-  productService = inject(ProductService);
-  deleteMessage: string | null = null;
-  //
-  ngOnInit() {
-    this.productService.listProducts().subscribe({
-      next: (products) => {
-        this.products = products;
-      },
-      error: (error) => {
-        // show error
-        console.error(error.message);
-      },
-    });
-  }
+    products: Product[] = [];
+    filteredProducts: Product[] = [];
+    productService = inject(ProductService);
+    deleteMessage: string | null = null;
+    //
+    ngOnInit() {
+      this.productService.listProducts().subscribe({
+        next: (products) => {
+          this.products = products;
+        },
+        error: (error) => {
+          // show error
+          console.error(error.message);
+        },
+      });
+    }
 
-  //
+    //
 
   handleDeleteProduct(id: any) {
     this.deleteMessage = 'Xoá thành công !';
     setTimeout(() => {
-      this.productService.deleteProduct(id).subscribe({
+      this.productService.deleteProduct(id).subscribe({ 
         next: () => {
           this.products = this.products.filter((product) => product.id !== id);
           this.filteredProducts = this.filteredProducts.filter(
